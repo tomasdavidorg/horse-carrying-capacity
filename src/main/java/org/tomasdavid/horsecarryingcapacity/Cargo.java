01b1b81d73a3d6016854d17895ad597a06491180
Tomas David <tdavid@redhat.com>
2014-08-05 10:47:27 +0200

    added classes Horse and Cargo

---
+++ src/main/java/org/tomasdavid/horsecarryingcapacity/Cargo.java
@@ -0,0 +1,25 @@
+package org.tomasdavid.horsecarryingcapacity;
+
+
+public class Cargo {
+
+    private int weight;
+
+    private Horse horse;
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public Horse getHorse() {
+        return horse;
+    }
+
+    public void setHorse(Horse horse) {
+        this.horse = horse;
+    }
+}
---
+++ src/main/java/org/tomasdavid/horsecarryingcapacity/Horse.java
@@ -0,0 +1,18 @@
+package org.tomasdavid.horsecarryingcapacity;
+
+public class Horse {
+
+    private int carryingCapacity;
+
+    public Horse(int carryingCapacity) {
+        this.carryingCapacity = carryingCapacity;
+    }
+
+    public int getCarryingCapacity() {
+        return carryingCapacity;
+    }
+
+    public void setCarryingCapacity(int carryingCapacity) {
+        this.carryingCapacity = carryingCapacity;
+    }
+}
